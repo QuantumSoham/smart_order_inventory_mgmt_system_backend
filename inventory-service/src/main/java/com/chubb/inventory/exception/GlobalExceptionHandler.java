e7b43e32b2916dbd2103801e52345ea8e7feeb68
QuantumSoham <quantumsc03@gmail.com>
2025-12-30 23:46:48 +0530

    added Global Exception Handler

---
+++ inventory-service/src/main/java/com/chubb/inventory/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.chubb.inventory.exception;
+
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<?> handle(BusinessException ex) {
+        return ResponseEntity.badRequest()
+                .body(Map.of("error", ex.getMessage()));
+    }
+}
