7e1d4aea61da5d2ef718e47481fe4fa2d81e222b
QuantumSoham <quantumsc03@gmail.com>
2025-12-30 16:33:53 +0530

    added Inventory repository

---
+++ inventory-service/src/main/java/com/chubb/inventory/repository/InventoryRepository.java
@@ -0,0 +1,29 @@
+package com.chubb.inventory.repository;
+
+
+
+import com.chubb.inventory.entity.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+    // Used in reserve / release / commit
+    Optional<Inventory> findByProductIdAndWarehouseId(
+            Long productId,
+            Long warehouseId
+    );
+
+    // GET /inventory/warehouse/{warehouseId}
+    List<Inventory> findByWarehouseId(Long warehouseId);
+
+    // GET /inventory/product/{productId}
+    List<Inventory> findByProductId(Long productId);
+
+    // Used for low-stock detection (optional optimization)
+    List<Inventory> findByAvailableQuantityLessThanEqual(
+            int threshold
+    );
+}
