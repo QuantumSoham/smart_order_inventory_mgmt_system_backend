1603da871b33e65a1041b1afd215ec851fba5350
QuantumSoham <quantumsc03@gmail.com>
2025-12-31 00:49:01 +0530

    added product entity

---
+++ inventory-service/src/main/java/com/chubb/inventory/entity/Product.java
@@ -0,0 +1,27 @@
+package com.chubb.inventory.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(length = 1000)
+    private String description;
+
+    private String category;
+
+    private String imageUrl;
+
+    private boolean active = true;
+}
