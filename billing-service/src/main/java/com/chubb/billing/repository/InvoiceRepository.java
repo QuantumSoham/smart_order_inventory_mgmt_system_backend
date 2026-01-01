48458ecb3f87463bf410c53b056dbc778fda09c1
QuantumSoham <quantumsc03@gmail.com>
2026-01-01 22:27:51 +0530

    Create InvoiceRepository.java

---
+++ billing-service/src/main/java/com/chubb/billing/repository/InvoiceRepository.java
@@ -0,0 +1,11 @@
+package com.chubb.billing.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.chubb.billing.entity.Invoice;
+
+public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+    Optional<Invoice> findByOrderId(Long orderId);
+}
