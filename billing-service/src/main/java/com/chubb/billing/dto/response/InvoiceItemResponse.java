f13b1798ed604a786ce9bb84c6c489d1425a6c63
QuantumSoham <quantumsc03@gmail.com>
2026-01-01 22:26:06 +0530

    Update InvoiceItemResponse.java

--- billing-service/src/main/java/com/chubb/billing/dto/response/InvoiceItemResponse.java
+++ billing-service/src/main/java/com/chubb/billing/dto/response/InvoiceItemResponse.java
@@ -1,5 +1,16 @@
 package com.chubb.billing.dto.response;
 
+import java.math.BigDecimal;
+
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
 public class InvoiceItemResponse {
 
+    private String productName;
+    private String category;
+    private int quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal lineTotal;
 }
