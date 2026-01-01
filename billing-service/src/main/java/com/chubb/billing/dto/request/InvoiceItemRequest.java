386edb4bcd49f051cdec4ee57e37418b2c200d55
QuantumSoham <quantumsc03@gmail.com>
2026-01-01 22:23:43 +0530

    Update InvoiceItemRequest.java

--- billing-service/src/main/java/com/chubb/billing/dto/request/InvoiceItemRequest.java
+++ billing-service/src/main/java/com/chubb/billing/dto/request/InvoiceItemRequest.java
@@ -1,5 +1,16 @@
 package com.chubb.billing.dto.request;
 
+import java.math.BigDecimal;
+
+import lombok.*;
+
+@Getter @Setter
 public class InvoiceItemRequest {
 
+    private Long productId;
+    private String productName;
+    private String category;
+    private int quantity;
+    private BigDecimal unitPrice;
 }
+
