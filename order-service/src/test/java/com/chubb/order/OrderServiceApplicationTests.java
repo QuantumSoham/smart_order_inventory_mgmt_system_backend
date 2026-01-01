e05f5fb57d2131e891190743e563158c9a0729ee
QuantumSoham <quantumsc03@gmail.com>
2026-01-01 15:03:24 +0530

    initialized order microservice

---
+++ order-service/src/test/java/com/chubb/order/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chubb.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
