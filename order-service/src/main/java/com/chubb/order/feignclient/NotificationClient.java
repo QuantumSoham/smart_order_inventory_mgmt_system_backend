d21379ff2507a63e0e1ece0febd34cfb960e9cde
QuantumSoham <quantumsc03@gmail.com>
2026-01-07 02:12:44 +0530

    added config server, registered kafka with eureka

--- order-service/src/main/java/com/chubb/order/feignclient/NotificationClient.java
+++ order-service/src/main/java/com/chubb/order/feignclient/NotificationClient.java
@@ -6,8 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(
-    name = "notification-service",
-    url = "http://localhost:9086" // Kafka demo app port
+    name = "notification-service"
 )
 public interface NotificationClient {
 
